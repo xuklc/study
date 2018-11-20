d9611aef7a25e9a3aee9615c50d937b8eb6a9a4b
xkl <1652562971@qq.com>
2018-11-20 15:49:35 +0800

    删除

--- springcloudDemo/gateway/src/main/java/com/nx/springcloud/com/nx/test/generate/Test.java
+++
@@ -1,9 +0,0 @@
-package com.nx.springcloud.com.nx.test.generate;
-
-/**
- * @author xukl
- * @date 2018/11/6
- */
-public class Test<O> {
-
-}
--- springcloudDemo/note.md
+++
@@ -1,7 +0,0 @@
-1gateway
-2 login
-3 product
-4 store
-
-
-
